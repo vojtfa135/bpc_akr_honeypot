b7858a8906bc701e20bf3cac8e2e8d911c7f6db1
honey <honey@opencanary-honeypot.0.2.15>
2020-12-04 22:32:42 +0100

    Lotta changes

---
+++ demo/conn_to_telnet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+telnet -p ; 127.0.0.1
--- demo/scan.sh
+++ demo/scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ip=$1
-ports=(21 22 25 53 80 110 123 143 443 465 631 993 995)
+ports=(21 22 23 25 53 80 110 123 143 443 465 631 993 995)
 
 # scan for commonly open ports
 for port in ${ports[@]};
